7c5bba44e3b363d245101524e7a92bdda7355b93
VeraLaureano <laureanoivangerardo@gmail.com>
2023-09-24 14:30:07 -0300

    24/09/23 - actualizacion del readme

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
-# 422WRLD
-### Dripping flow
+# **422WRLD - API**
+
+## **Basic Guide**
+- - -
+## *Commands*
+- Install dependencies
+```bash 
+npm install
+```
+- Lint
+```bash
+npm run lint
+```
+
+- Build
+```bash
+npm run build
+```
+
+- Start Commands:
+```bash
+npm run start
+```
+```bash
+npm run start:prod
+```
+```bash
+npm run start:dev
+```
+
+## *Routing API*
+- **Home** *(Web Service)*: *https://four22-wrld.onrender.com/* 
+- **Home** *(Local Host)*: *http://localhost:800/*
+    > METHOD: *POST*
+    - **User *signup***: */api/v1/user/signup*
+    - **User *login***: */api/v1/user/login*
+    > METHOD: *GET*
+    - **Artists**: */api/v1/artists*
+    - **Find one Artist**: */api/v1/artists/:id*
+    - **Albums**: */api/v1/albums*
+    - **Find one Album**: */api/v1/albums/:id*
+    - **Songs**: */api/v1/songs*
+    - **Find one Song**: */api/v1/song/:id*
+    - **Api Docs**: */api-docs*
+
+## *Project Structure*
+- ./src <br />
+    |--> /config <br />
+    |--> /controllers <br />
+    |--> /docs <br />
+    |--> /interfaces <br />
+    |--> /middleware <br />
+    |--> /models <br />
+    |--> /routes <br />
+    |--> /services <br />
+    |--> /utils <br />
+    |--> /app.ts <br />
+    |--> /index.ts *(main file)* <br />
--- src/docs/paths.ts
+++ src/docs/paths.ts
@@ -1,18 +1,9 @@
-import { deleteAlbumDocV1 } from './album/delete-album'
 import { getOneAlbumDocV1 } from './album/get-album'
 import { getAllAlbumDocV1 } from './album/get-albums'
-import { patchAlbumDocV1 } from './album/patch-album'
-import { postAlbumsDocV1 } from './album/post-album'
-import { deleteArtistDocV1 } from './artist/delete-artist'
 import { getOneArtistDocV1 } from './artist/get-artist'
 import { getAllArtistsDocV1 } from './artist/get-artists'
-import { patchArtistDocV1 } from './artist/patch-artist'
-import { postArtistDocV1 } from './artist/post-artist'
-import { deleteSongDocV1 } from './song/delete-song'
 import { getOneSongDocV1 } from './song/get-song'
 import { getAllSongsDocV1 } from './song/get-songs'
-import { patchSongDocV1 } from './song/patch-song'
-import { postSongDocV1 } from './song/post-song'
 import { postLoginUserDocV1 } from './user/login-user'
 import { postSignupUserDocV1 } from './user/signup-user'
 
@@ -41,27 +32,6 @@ export const docsPaths = {
     },
     '/api/v1/artists/{id}': {
       ...getOneArtistDocV1
-    },
-    '/api/v1/admin/songs': {
-      ...postSongDocV1
-    },
-    '/api/v1/admin/songs/{id}': {
-      ...patchSongDocV1,
-      ...deleteSongDocV1
-    },
-    '/api/v1/admin/albums': {
-      ...postAlbumsDocV1
-    },
-    '/api/v1/admin/albums/{id}': {
-      ...patchAlbumDocV1,
-      ...deleteAlbumDocV1
-    },
-    '/api/v1/admin/artists': {
-      ...postArtistDocV1
-    },
-    '/api/v1/admin/artists/{id}': {
-      ...patchArtistDocV1,
-      ...deleteArtistDocV1
-    },
+    }
   }
 }
